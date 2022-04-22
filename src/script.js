91ed3ad51f49e6522e074f82e685d8c65d30e9db
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-22 03:18:53 +0300

    add profile functionality

--- src/script.js
+++ src/script.js
@@ -1,15 +1,34 @@
 //require("dotenv").config()
 
 let home = document.getElementById("home");
-let homeDiv = document.querySelector(".home-container");
 let login = document.getElementById("login");
 let register = document.getElementById("register");
 let signinForm = document.querySelector(".signin");
 let signupForm = document.querySelector(".signup");
 let admin = document.getElementById("admin");
 let adminDiv = document.querySelector(".admin-container");
+let profileDiv = document.querySelector(".profile-container");
+
+profile.addEventListener("click", () => {
+  profile.children[0].classList.add("active");
+  profileDiv.removeAttribute("id");
+  login.children[0].classList.remove("active");
+  register.children[0].classList.remove("active");
+  home.children[0].classList.remove("active");
+  admin.children[0].classList.remove("active");
+  homeDiv.classList.add("home-container-hidden");
+  homeDiv.classList.remove("d-flex", "flex-column");
+  adminDiv.classList.remove("d-flex", "flex-column");
+  adminDiv.classList.add("admin-container-hidden");
+  signupForm.classList.remove("signup-div");
+  signupForm.classList.add("signup-hidden");
+  signinForm.classList.remove("signin-div");
+  signinForm.classList.add("signin-hidden");
+});
 
 admin.addEventListener("click", () => {
+  profile.children[0].classList.remove("active");
+  profileDiv.setAttribute("id","profile-container");
   login.children[0].classList.remove("active");
   register.children[0].classList.remove("active");
     home.children[0].classList.remove("active");
@@ -25,6 +44,8 @@ admin.addEventListener("click", () => {
 });
 
 home.addEventListener("click", () => {
+  profile.children[0].classList.remove("active");
+  profileDiv.setAttribute("id", "profile-container");
     admin.children[0].classList.remove("active");
   login.children[0].classList.remove("active");
   register.children[0].classList.remove("active");
@@ -39,6 +60,8 @@ home.addEventListener("click", () => {
   signinForm.classList.add("signin-hidden");
 });
 login.addEventListener("click", () => {
+  profile.children[0].classList.remove("active");
+  profileDiv.setAttribute("id", "profile-container");
     admin.children[0].classList.remove("active");
   login.children[0].classList.add("active");
   register.children[0].classList.remove("active");
@@ -55,6 +78,8 @@ login.addEventListener("click", () => {
   signinForm.classList.add("signin-div");
 });
 register.addEventListener("click", () => {
+  profile.children[0].classList.remove("active");
+  profileDiv.setAttribute("id", "profile-container");
     admin.children[0].classList.remove("active");
   login.children[0].classList.remove("active");
   register.children[0].classList.add("active");
